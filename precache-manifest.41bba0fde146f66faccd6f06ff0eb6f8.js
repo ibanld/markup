c4d654600f68a4cdfe9dc2959ac452bfee42cfce
Ibán López <ibanlopezd@gmail.com>
2020-08-15 13:56:13 +0200

    Updates

---
+++ precache-manifest.41bba0fde146f66faccd6f06ff0eb6f8.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7092a965dd35247a5318411c31d53e0",
+    "url": "/markup/index.html"
+  },
+  {
+    "revision": "aafcb3d9927da98ff93d",
+    "url": "/markup/static/css/main.3283f4a2.chunk.css"
+  },
+  {
+    "revision": "1ff5057fefe901f94bee",
+    "url": "/markup/static/js/2.06c1fbae.chunk.js"
+  },
+  {
+    "revision": "274fdd3f7f2c02eb9129413c42456716",
+    "url": "/markup/static/js/2.06c1fbae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "caaff8418ba968bfdc1f",
+    "url": "/markup/static/js/3.0a5483e9.chunk.js"
+  },
+  {
+    "revision": "287e48aeac3564a8b2b5685a67fb9216",
+    "url": "/markup/static/js/3.0a5483e9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aafcb3d9927da98ff93d",
+    "url": "/markup/static/js/main.5c52a364.chunk.js"
+  },
+  {
+    "revision": "a33f0340de7de0ba4346",
+    "url": "/markup/static/js/runtime-main.35b114d3.js"
+  },
+  {
+    "revision": "2624d257d041019f8b514f254a51bc19",
+    "url": "/markup/static/media/macstart.2624d257.m4a"
+  },
+  {
+    "revision": "598e97eea1b4e879e3fe76f2e8d3604c",
+    "url": "/markup/static/media/winstart.598e97ee.m4a"
+  }
+]);
