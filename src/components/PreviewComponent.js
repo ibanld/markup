c7e6921419130f23d478524651f98aea39ecb558
Ibán López <ibanlopezd@gmail.com>
2020-08-15 12:24:37 +0200

    Preview component finished

--- src/components/PreviewComponent.js
+++ src/components/PreviewComponent.js
@@ -1,19 +1,28 @@
 import React from 'react';
 import marked from 'marked';
-
 import { Window, TitleBar, Text } from 'react-desktop/macOs';
+import { TitleBar as WinTitleBar, Text as WinText, Window as WinWindow } from 'react-desktop/windows';
 
-export default function Preview({ text }) {
+export default function Preview({ text, win }) {
 	return (
-		<Window chrome height='50%' width='45%' padding='1%' id='preview'>
-			<TitleBar
-				title='Preview'
-				controls
-				onCloseClick={() => console.log('Close window')}
-				onMinimizeClick={() => console.log('Minimize window')}
-				onMaximizeClick={() => console.log('Mazimize window')}
-			/>
-			<Text dangerouslySetInnerHTML={{ __html: marked(text) }} />
-		</Window>
+        <>
+            {!win ? (
+                <Window chrome height='50%' width='45%' padding='1%' id='preview'>
+                    <TitleBar
+                        title='Preview'
+                        controls
+                    />
+                    <Text dangerouslySetInnerHTML={{ __html: marked(text) }} />
+                </Window>
+            ) : (
+                <WinWindow chrome height='50%' width='45%' padding='0%' theme='dark' background='white' id='win-preview'>
+                    <WinTitleBar
+                        title='Preview'
+                        controls
+                    />
+                    <WinText dangerouslySetInnerHTML={{ __html: marked(text) }}/>
+                </WinWindow>
+            )}
+        </>
 	);
 }
